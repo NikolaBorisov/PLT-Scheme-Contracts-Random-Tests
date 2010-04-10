c0a7dfb47a9856ef37a208bcf1428f0e076a1ba3
Nikola Borisov <nikola@nikola-laptop.(none)>
2010-04-10 13:22:49 -0500

    New meaning of gen--> is not in place

--- contract-gen.scm
+++ contract-gen.scm
@@ -1,76 +1,69 @@
-(module contract-gen scheme
-  
-  (require (planet schematics/schemeunit:3:4))
-  
-  (define h (make-hash))
-  
-  (define (gen-integer)
-    (λ ()
-      (- 50 (random 100))))
-  
-  (hash-set! h integer? (gen-integer))
-  
-  (define (gen-positive)
-    (λ ()
-      (+ (abs ((gen-integer))) 1)))
-  
-  (hash-set! h positive? (gen-positive))
-  
-  (define (gen-between/c n m)
-    (λ ()
-      (+ (* (- m n) (random)) n)))
-  
-  (hash-set! h (between/c 0 10) (gen-between/c 0 10))
-  
-  (define (gen contract)
-    (let ([g (hash-ref h contract #f)])
-      (if g
-          g
-          (error "Can not generate value for contract ~a" contract))))
-  
- 
-  
-  (define (test-gen-->0.1)
-    0)
-  (define (test-gen-->0.2 an-integer)
-    0)
-  (define (test-gen-->0.3 an-integer a-positive)
-    0)
-  (define (test-gen-->0.4 a-between/c-0-10)
-    a-between/c-0-10)
-  
-  (provide/contract
-   [gen-integer (-> (-> integer?))]
-   [gen-positive (-> (-> positive?))]
-   [gen-between/c (->d
-                   ([n integer?]
-                    [m (and/c integer? (>=/c n))]) 
-                   ()
-                   [result (-> (between/c n m))])]
-   [test-gen-->0.1 (-> any)]
-   [test-gen-->0.2 (-> integer? any)]
-   [test-gen-->0.3 (-> integer? positive? any)]
-   [test-gen-->0.4 (-> (between/c 0 10) (between/c 0 10))]
-   )
-  
-  (provide gen-->)
-  
-  
-  
-  (define (gen--> . args)
-    (let ([f-args-c (reverse (rest (reverse args)))]
-          [return-value-c (first (reverse args))])
-      (let ([f-args-gen (map gen f-args-c)])
-        (λ ()
-          ;          (length f-args-gen)))))
-          (map (λ (x)
-                 (apply x (list)))
-               f-args-gen)))))
-  
-  
-  
-  ;;(test-gen between/c 10 (λ ()
-  ;;                         (test-fun ((gen between/c) 0 10))))
-  
-  
-  )
+#lang scheme
+
+(require (planet schematics/schemeunit:3:4))
+
+(define h (make-hash))
+
+(define (gen-integer)
+  (λ (depth)
+    (- 50 (random 100))))
+
+(hash-set! h integer? (gen-integer))
+
+(define (gen-positive)
+  (λ (depth)
+    (+ (abs ((gen-integer) depth)) 1)))
+
+(hash-set! h positive? (gen-positive))
+
+(define (gen-between/c n m)
+  (λ (depth)
+    (+ (* (- m n) (random)) n)))
+
+(hash-set! h (between/c 0 10) (gen-between/c 0 10))
+
+(define (gen contract)
+  (let ([g (hash-ref h contract #f)])
+    (if g
+        g
+        (error "Can not generate value for contract ~a" contract))))
+
+
+(define (gen--> dom range)
+  (λ (depth)
+    (λ (x)
+      ((gen range) (- depth 1)))))
+
+
+(provide/contract
+ [gen-integer (-> (-> integer? integer?))]
+ [gen-positive (-> (-> integer? positive?))]
+ [gen-between/c (->d
+                 ([n integer?]
+                  [m (and/c integer? (>=/c n))]) 
+                 ()
+                 [result (-> integer? (between/c n m))])]
+ [gen--> (->d ([d contract?]
+               [r contract?])
+              ()
+              [result (-> integer? (-> d r))])]
+ )
+
+(provide gen)
+
+
+
+
+#|
+(define (gen--> . args)
+  (let ([f-args-c (reverse (rest (reverse args)))]
+        [return-value-c (first (reverse args))])
+    (let ([f-args-gen (map gen f-args-c)])
+      (λ ()
+        ;          (length f-args-gen)))))
+        (map (λ (x)
+               (apply x (list)))
+             f-args-gen)))))
+
+|#
+
--- test.ss
+++ test.ss
@@ -5,15 +5,16 @@
 (define (test-gen-s g attempts)
   (if (<= attempts 0)
       #t
-      (begin (g)
+      (begin (g 2)
              (test-gen-s g (- attempts 1)))))
 
-(define (test-gen g f attempts)
-  (if (<= attempts 0)
-      #t
-      (begin (apply f (g))
-             (test-gen-s g (- attempts 1)))))
-
+(define (test-gen--> dom range attempts)
+  (local ([define g (gen--> dom range)]
+          [define dom-gen (gen dom)])
+    (do ([x 0 (+ x 1)])
+      ((>= x attempts))
+      ((g 2) (dom-gen 2)))
+    #t))
 
 (define (make/c contract . args)
   (void))
@@ -28,11 +29,9 @@
 (check-equal? (test-gen-s (gen-between/c 0 100) 100)
               #t)
 
-(check-equal? (test-gen (gen--> integer?) test-gen-->0.1 100)
+(check-equal? (test-gen--> integer? integer? 100)
               #t)
-(check-equal? (test-gen (gen--> integer? integer?) test-gen-->0.2 100)
+
+(check-equal? (test-gen--> integer? positive? 100)
               #t)
-(check-equal? (test-gen (gen--> integer? positive? integer?) test-gen-->0.3 100)
-              #t)
-(check-equal? (test-gen (gen--> (between/c 0 10) (between/c 0 10)) test-gen-->0.4 100)
-              #t)
+
